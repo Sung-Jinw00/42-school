2d7d0118da750615cb20ecaaf61d4513b84e9e86
Louis Cagnion <locagnio@c2r7p3.42perpignan.fr>
2025-02-02 20:00:46 +0100

    philo bonus meh

--- exercices/Cursus/Cercle 4/minishell/main.c
+++ exercices/Cursus/Cercle 4/minishell/main.c
@@ -6,32 +6,17 @@
 /*   By: locagnio <locagnio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 18:03:17 by locagnio          #+#    #+#             */
-/*   Updated: 2025/01/25 18:46:10 by locagnio         ###   ########.fr       */
+/*   Updated: 2025/02/02 19:59:46 by locagnio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "minishell.h"
 
 //compile avec -lreadline pour avoir les fonctions de readline
 //le bash doit fonctionner sur bash, pas zsh
 
 int main()
 {
-    char *line = readline("Entrez quelque chose : ");
-    
-    if (line)
-	{
-        // Remplacer la ligne actuelle par un texte différent
-        rl_replace_line("Ligne remplacée!", 0);
-        
-        // Afficher la nouvelle ligne après remplacement
-        printf("Ligne après remplacement : %s\n", rl_line_buffer);
-        
-        free(line);  // Libérer la mémoire allouée
-    }
-    
-    return 0;
+	
+	return (0);
 }
--- exercices/Cursus/Cercle 4/minishell/minishell.h
+++ exercices/Cursus/Cercle 4/minishell/minishell.h
@@ -6,13 +6,16 @@
 /*   By: locagnio <locagnio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 18:03:29 by locagnio          #+#    #+#             */
-/*   Updated: 2025/01/25 18:42:20 by locagnio         ###   ########.fr       */
+/*   Updated: 2025/02/02 19:59:34 by locagnio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 # endif
